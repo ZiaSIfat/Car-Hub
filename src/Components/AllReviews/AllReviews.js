168f72cc869da025bd1c1ea0471dc1553c6a4f36
ZiaSifat <ziasifat150@gmail.com>
2022-04-06 14:52:49 +0600

    review done

--- src/Components/AllReviews/AllReviews.js
+++ src/Components/AllReviews/AllReviews.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './AllReviews.css';
 const AllReviews = (props) => {
-    const { name, about, picture } = props.review;
+    const { name, about, picture, rating } = props.review;
     return (
         <div className='review '>
             <img className='m-4' src={picture} alt="" />
             <h2>Name: {name}</h2>
             <p>{about}</p>
+            <p><small>Rating: {rating}</small></p>
         </div>
 
     );
