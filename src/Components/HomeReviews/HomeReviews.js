4b8ed6e77209d32fba20ec474c8b998ffad6e383
ZiaSifat <ziasifat150@gmail.com>
2022-04-06 19:07:59 +0600

    some customization

--- src/Components/HomeReviews/HomeReviews.js
+++ src/Components/HomeReviews/HomeReviews.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './HomeReviews.css';
 
 const HomeReviews = (props) => {
-    const { name, picture, about } = props.review;
+    const { name, picture, about, rating } = props.review;
     return (
         <div className='home-review'>
             <img className='m-5' src={picture} alt="" />
-            <h3>{name}</h3>
+            <h2>Name: {name}</h2>
             <p>{about}</p>
+            <p><small>Rating: {rating}</small></p>
         </div>
 
     );
